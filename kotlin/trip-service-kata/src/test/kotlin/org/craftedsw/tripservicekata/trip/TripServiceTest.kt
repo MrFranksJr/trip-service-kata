1d3ea30be812f2ff9c6b5ec1fa42a736510bab40
Franky Jr Blondeel <frankyjr@me.com>
2024-10-11 16:25:52 +0200

    finally got dependencies right and ran first test

--- kotlin/trip-service-kata/src/test/kotlin/org/craftedsw/tripservicekata/trip/TripServiceTest.kt
+++ kotlin/trip-service-kata/src/test/kotlin/org/craftedsw/tripservicekata/trip/TripServiceTest.kt
@@ -1,5 +1,24 @@
 package org.craftedsw.tripservicekata.trip
 
+import io.mockk.*
+import org.craftedsw.tripservicekata.exception.UserNotLoggedInException
+import org.craftedsw.tripservicekata.user.User
+import org.craftedsw.tripservicekata.user.UserSession
+import kotlin.test.Test
+import kotlin.test.assertFailsWith
+
 class TripServiceTest {
 
+    @Test
+    fun `should throw UserNotLoggedInException when user is not logged in`() {
+        val tripService = TripService()
+        val user = mockk<User>()
+        mockkObject(UserSession)
+
+        every { UserSession.instance.loggedUser } returns null
+
+        assertFailsWith<UserNotLoggedInException> {
+            tripService.getTripsByUser(user)
+        }
+    }
 }
