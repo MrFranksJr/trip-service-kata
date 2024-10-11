60e43b7ae66d1d535de9e02af14633930cc57c23
Franky Jr Blondeel <frankyjr@me.com>
2024-10-11 18:08:35 +0200

    migrate to junit 5

--- kotlin/trip-service-kata/src/test/kotlin/org/craftedsw/tripservicekata/trip/TripServiceTest.kt
+++ kotlin/trip-service-kata/src/test/kotlin/org/craftedsw/tripservicekata/trip/TripServiceTest.kt
@@ -5,11 +5,13 @@ import io.mockk.mockkObject
 import org.craftedsw.tripservicekata.exception.UserNotLoggedInException
 import org.craftedsw.tripservicekata.user.User
 import org.craftedsw.tripservicekata.user.UserSession
-import org.junit.Before
-import kotlin.test.Test
-import kotlin.test.assertFailsWith
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
-class TripServiceTest() {
+class TripServiceTest {
     private lateinit var tripService: TripService
     private val guestUser: User = User()
     private var friendUser: User = User()
@@ -19,7 +21,7 @@ class TripServiceTest() {
     private val tripToJapan = Trip()
     private val tripList: List<Trip> = listOf(tripToGreece, tripToJapan)
 
-    @Before
+    @BeforeEach
     fun init() {
         tripService = TripService()
     }
@@ -30,9 +32,7 @@ class TripServiceTest() {
 
         every { UserSession.instance.loggedUser } returns null
 
-        assertFailsWith<UserNotLoggedInException> {
-            tripService.getTripsByUser(guestUser)
-        }
+        assertThrows<UserNotLoggedInException> { tripService.getTripsByUser(guestUser) }
     }
 
     @Test
@@ -45,7 +45,7 @@ class TripServiceTest() {
         friendUser.addFriend(anotherUser)
         friendUser.addTrip(Trip())
 
-        assert(tripService.getTripsByUser(friendUser).isEmpty())
+        assertTrue { tripService.getTripsByUser(friendUser).isEmpty() }
     }
 
     @Test
@@ -59,7 +59,7 @@ class TripServiceTest() {
         friendUser.addTrip(tripToJapan)
         friendUser.addTrip(tripToGreece)
 
-        assert(tripService.getTripsByUser(friendUser).size == tripList.size)
-        assert(tripService.getTripsByUser(friendUser).containsAll(tripList))
+        assertEquals(2,tripService.getTripsByUser(friendUser).size)
+        assertTrue { tripService.getTripsByUser(friendUser).containsAll(tripList) }
     }
 }
